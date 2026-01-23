31aa5099f05685f526795898758ba9eaea164a61
nikkotech23-create <nikkotech23@gmail.com>
2026-01-23 17:12:23 +0000

    Update Team_Analytics.py

--- nfldashboard/pages/Team_Analytics.py
+++ nfldashboard/pages/Team_Analytics.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from core.data_loader import load_weekly, load_teams
+
+st.title("📊 Team Analytics")
+
+season = st.sidebar.selectbox("Season", [2024, 2023, 2022])
+team = st.sidebar.selectbox("Team", ["KC", "BUF", "PHI", "SF", "DAL"])
+
+wwekly = load_weekly([season])
+teams = load_teams()
+
+team_stats = weekly[weekly["team"] == team]
+
+st.subheader(f"{team} — Weekly EPA ({season})")
+st.dataframe(team_stats[["week", "epa"]])
+
+
+
+
 
